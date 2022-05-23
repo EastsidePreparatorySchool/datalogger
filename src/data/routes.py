46d19d20034c184e8260591af5b28d65b530e7e8
Jonathan <jonathan.briggs@gmail.com>
2022-05-23 14:32:49 -0700

    changing from request.data to request.json

--- src/data/routes.py
+++ src/data/routes.py
@@ -48,8 +48,8 @@ def auth_callback():
 def form_or_args(request):
     if request.form:
         params = request.form
-    elif request.data:
-        params = request.data
+    elif request.json:
+        params = request.get_json
     else:
         params = request.args
     return params
